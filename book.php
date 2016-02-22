27768b8cdae26494006da8cb20da5089d8619ce1
PetarAD <petar.ad@mail.bg>
2016-02-22 22:38:41 +0200

    for fun
    just playing

---
+++ book.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Book</title>
+  </head>
+  <body>
+    //Примерен файл
+        <header>
+          <h1>
+            PHP BOOK
+          </h1>
+        </header>
+        //Създаване на профил
+        <form action="book.php" method="post">
+          <input type="text" name="username">
+          <input type="password" name="password">
+          <input type="submit" name="submit" value="Потвърди">
+        </form>
+        <footer>
+          <h2>
+            PHP BOOK
+          </h2>
+        </footer>
+  </body>
+</html>
