887c586d112f7cc2cd22742aca5322fc04c43c34
PetarAD <petar.ad@mail.bg>
2016-04-02 23:14:41 +0300

    change
    change

--- exams.php
+++ exams.php
@@ -4,6 +4,14 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="read.css"  charset="utf-8">
     <title>Exams</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </head>
   <body>
     <form  action="exams.php" method="get">
@@ -31,9 +39,8 @@
   $read_result = mysqli_query($conn, $read_query);
     	if (mysqli_num_rows($read_result) > 0) {
     		while($row = mysqli_fetch_assoc($read_result)){
-    		echo $row["exam"]."<hr>";
-
-    		}
+        echo '<p class="bg-danger">'. $row["exam"].'</p>';
+  		}
     	}
 
     }
